a81fdbe959b9a91c473f3fd706a63e29bbe94851
nick leavitt <nick.leavitt@westrock.com>
2024-07-30 15:11:43 -0400

    Arg parsing is functional in decibelnorm.py. This allows you to specify how many columns you want to normalize.

--- decibelnorm.py
+++ decibelnorm.py
@@ -9,19 +9,19 @@ from operations import select_file as selectFile
 def parserInit():
     parser = argparse.ArgumentParser(description='Normalize decibel readings to 1ft.')
     parser.add_argument('filename', help="File to normalize")
-    parser.add_argument('columns', help="Number of columns to normalize")
+    parser.add_argument('-c', '--columns', help="Number of columns to normalize", nargs='+', type = int)
     parser.add_argument('-s','--skipheader', action='store_true', help='Skip the header when normalizing data')
-    args = parser.parse_args(['C:/Users/nleavitt/Python/Acoustic_test/data_2.csv', 3 ])
-    #args = parser.parse_args()
+    #args = parser.parse_args(['C:/Users/nleavitt/Python/Acoustic_test/data_2.csv', '-c 0 2' ])
+    args = parser.parse_args()
     return args
 
 def errorLogic(args):
     file = args.filename
     cols = args.columns
-    if cols < 1:
-        argparse.ArgumentError
-        #print("No regression model selected.")
-        sys.exit("Cannot normalize 0 columns")
+    # if cols < 1:
+    #     argparse.ArgumentError
+    #     #print("No regression model selected.")
+    #     sys.exit("Cannot normalize 0 columns")
 
     if not file.endswith('.csv'):
         argparse.ArgumentError
@@ -32,20 +32,22 @@ def errorLogic(args):
 def dataPull(file, args):
     cols = args.columns
     header = args.skipheader
-    collist = []
-    for i in cols:
-        collist.append(i)
-    i=2
-    data = np.genfromtxt(file, delimiter=',',skip_header=header, usecols = collist)
+    # collist = []
+    # for i in cols:
+    #     collist.append(i)
+    #     i+=1
+    data = np.genfromtxt(file, delimiter=',',skip_header=header, usecols = cols)
     return data
 
 
 
 def main():
-    parserInit()
-    errorLogic()
     file = ""
-    file = dataExtract()
+    args = parserInit()
+    errorLogic(args)
+    file = args.filename
+    data = dataPull(file, args)
+    print(data)
 
 
 if __name__ == "__main__":
