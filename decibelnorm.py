72adb2d3e680b175ac37267c2fb2f25766118bee
nick leavitt <nick.leavitt@westrock.com>
2024-08-01 16:28:01 -0400

    decibel norm is functional now, only allows even amount of columns

--- decibelnorm.py
+++ decibelnorm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import sys
 import argparse
 
@@ -11,7 +12,7 @@ def parserInit():                                                               
     parser.add_argument('filename', help="File to normalize")
     parser.add_argument('-c', '--columns', help="Number of columns to normalize", nargs='+', type = int)
     parser.add_argument('-s','--skipheader', action='store_true', help='Skip the header when normalizing data')
-    #args = parser.parse_args(['C:/Users/nleavitt/Python/Acoustic_test/data_2.csv', '-c 0 2' ])
+    #args = parser.parse_args(['C:/Users/nleavitt/Python/Acoustic_test/data_2.csv', '-c', '0', '2', '-s'])
     args = parser.parse_args()
     return args
 
@@ -28,6 +29,8 @@ def errorLogic(args):                                                           
         #print('Invalid filetype, select a .csv file')
         sys.exit('Invalid filetype, select a .csv file')
 
+    if (len(cols) & 1):
+        sys.exit('Invalid number of colums, must be a multiple of 2')
 
 def dataPull(file, args):                                                   #pulling data from selected .csv
     cols = args.columns
@@ -39,6 +42,25 @@ def dataPull(file, args):                                                   #pul
     data = np.genfromtxt(file, delimiter=',',skip_header=header, usecols = cols)
     return data
 
+def dataNorm(data):
+    norm = np.zeros((data.shape[0], data.shape[1]//2))
+    for i in range (data.shape[1]-1):
+        for j in range (data.shape[0]):
+            norm[j,i] = data[j,i] +20*math.log10(data [j,i+1])
+            j+=1
+        i+=2
+    return norm
+
+def negativeCheck(data):
+    for i in range (data.shape[1]):
+        for j in range (data.shape[0]):
+            if data[j, i]<0:
+                print("Cannot process negative numbers")
+                return True
+            j+=1
+        i+=1
+    return False
+
 
 
 def main():
@@ -48,7 +70,8 @@ def main():
     file = args.filename
     data = dataPull(file, args)
     print(data)
-
-
+    if negativeCheck(data)==True: sys.exit("Cannot process negative numbers")
+    normalized = dataNorm(data)
+    print (normalized)
 if __name__ == "__main__":
     main()
