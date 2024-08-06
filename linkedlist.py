987b32ef996ce95f596be001c36202f64b995e7c
nick leavitt <nick.leavitt@westrock.com>
2024-08-06 11:21:59 -0400

    mostly finished branching functionality, just need to test. can add swap and print branch

--- linkedlist.py
+++ linkedlist.py
@@ -11,15 +11,18 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.headlist = None
+        self.branch = None
+        self.branchdict = None                      #creating a dict for branch/head pairs. the branch is the key and the head will be the value
     def insertTop(self, data):
         new_node = Node(data)
         if self.head is None:                       #if there is no head, the head becomes the new node
             self.head = new_node
+            self.branchdict['main'] = self.head     #updating the head stored in the branch dict. branchdict now holds the right pointer
             return
         else:                                       #else, this node now points to the old head and the new node becomes the head
             new_node.next = self.head
             self.head = new_node
+            self.branchdict['main'] = self.head
 
     def insertEnd(self, data):
         new_node = Node(data)
@@ -31,7 +34,7 @@ class LinkedList:
             current_node = current_node.next        #sets current node to the node its pointing to
         current_node.next = new_node                #points the current node towards the newly created node, because of the loop, this is at the end of the list
 
-    def insertIndex(self, data, index):
+    def insertIndex(self, data, index):             #index is counted from the head. if there is a branch, two indexes starting from different heads could access the same node
         if (index == 0):
             self.insertTop(data)
         current_node = self.head
@@ -60,6 +63,7 @@ class LinkedList:
     def deleteStart(self):
         if(self.head == None): return
         self.head = self.head.next
+        self.branchdict['main'] = self.head                     #update the head stored in branchdict
 
     def deleteEnd(self):
         if(self.head == None ): return
@@ -80,13 +84,13 @@ class LinkedList:
                 current_node.next = current_node.next.next
         else:print("No index found")
 
-    def printList(self):
+    def printBranch(self):                                          #iterates from head down, will only print the branch its on
         current_node = self.head
         while(current_node):
             print(current_node.data)
             current_node = current_node.next
 
-    def newBranch(self, data, index):
+    def newBranch(self, data, index, branchname):                   #branchname must be a string. will have to add error checking to ensure duplicate names are not entered
         if (index == 0):
             self.insertTop(data)
         current_node = self.head
@@ -97,7 +101,16 @@ class LinkedList:
         if (current_node != None):
             new_node = Node(data)
             new_node.next = current_node.next
+            self.branchdict[branchname] = new_node                 #stores branchname/new_node pair in branch dict. does NOT update head. branchChange method will swap head
         else:print("No index found")
+
+    def branchChange(self, branchname):
+        self.head = self.branchdict[branchname]
+        self.branch = branchname
+
+    def printHead(self):
+        print(self.branch)
+
         #make a list for each head and allow for swapping???
         #allow naming heads? how best to deal with this?
         #a dict with the name as the key? each dict val is the head of a seperate branch
