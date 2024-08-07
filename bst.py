cc40759eb3bdd660791e2d43d944ddf503037fa2
nick leavitt <nick.leavitt@westrock.com>
2024-08-07 14:21:51 -0400

    Continued work on BST, might need to restructure with a recursive architecture?

--- bst.py
+++ bst.py
@@ -7,7 +7,7 @@ class Node:
         self.left = None
         self.right = None
 
-class BinarySearchTree:
+class Tree:
     def __init__(self):
         self.root = None
 
@@ -20,9 +20,24 @@ class BinarySearchTree:
             print("Data already entered")
             return
         current_node = self.root
-        if new_node.data < self.root.data: next_node = new_node.left
+        if new_node.data < current_node.data: next_node = current_node.left
+        else: next_node = current_node.right
         while(next_node != None):
-            if (data < current_node.data and current_node.left!=None): current_node = current_node.left
-            if (data > current_node.data and current_node.right!=None): current_node = current_node.right
+            if new_node.data == current_node.data:                     #not allowing duplicate values
+                print("Data already entered")
+                return
+            if new_node.data < current_node.data: next_node = new_node.left
+            else: next_node = new_node.right
+            if next_node != None: current_node = next_node
+            #if (data < current_node.data and current_node.left!=None): current_node = current_node.left
+            #if (data > current_node.data and current_node.right!=None): current_node = current_node.right
+  
+        if (current_node.left is None and new_node.data < current_node.data): current_node.left = new_node
+        if (current_node.right is None and new_node.data > current_node.data): current_node.right = new_node
 
-           
+    def inOrder(self):
+        current_node = self.root
+        while(current_node != None):
+            if current_node is not None:
+                print (current_node.data)
+
